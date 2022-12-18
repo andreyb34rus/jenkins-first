796bb551d74a53181523d6841aec7b66c24c0090
andrey.barantsov <andrey.barantsov75@gmail.com>
2022-12-18 13:56:20 +0300

    Initial commit

---
+++ src/test/java/com/example/jenkinsfirst/JenkinsFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
